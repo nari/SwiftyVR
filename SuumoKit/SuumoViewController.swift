0c2331bb6f995dc20c307b097937115abd53e3ef
Nari <na.ju.rights@gmail.com>
2016-02-08 13:56:01 +0900

    add sphere object

--- SuumoKit/SuumoViewController.swift
+++ SuumoKit/SuumoViewController.swift
@@ -36,15 +36,27 @@ class SuumoViewController: UIViewController {
         let height = CGFloat(150)
         let width = CGFloat(50)
         
-        let tubeGeometry = SCNTube(innerRadius: width, outerRadius: width, height: height)
+//        let tubeGeometry = SCNTube(innerRadius: width, outerRadius: width, height: height)
+//        tubeNode = SCNNode(geometry: tubeGeometry)
+//        tubeNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
+//        tubeNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
+//        
+//        tubeGeometry.firstMaterial?.diffuse.contents = UIImage(named: "01.jpg")
+//        scene.rootNode.addChildNode(tubeNode)
+        
+        let tubeGeometry = SCNSphere(radius: 10.0)
         tubeNode = SCNNode(geometry: tubeGeometry)
         tubeNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
-        tubeNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
         
         tubeGeometry.firstMaterial?.diffuse.contents = UIImage(named: "01.jpg")
+        tubeGeometry.firstMaterial?.doubleSided = true
+        tubeGeometry.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(-1,1,1);
+        tubeGeometry.firstMaterial?.diffuse.wrapS = SCNWrapMode.Repeat
+        
         scene.rootNode.addChildNode(tubeNode)
+        
+        
         cameraNode.position = SCNVector3(x:0, y:0, z: 0)
-        cameraNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
         
         let scnView = self.view as! SCNView
         scnView.scene = scene
@@ -67,12 +79,14 @@ class SuumoViewController: UIViewController {
         let pointDiff = CGPointMake(pointNow.x - pointBefore.x, pointNow.y - pointBefore.y)
         
         lotateX = (accuracy * Float(pointDiff.x) + lotateX) % Float(M_PI*2)
-        tubeNode.rotation = SCNVector4(x:0, y:0, z:1, w: lotateX)
+        tubeNode.rotation = SCNVector4(x:0, y:-1, z:0, w: lotateX)
         
-        if (abs((accuracy * Float(pointDiff.y) + lotateY)) < 0.3){
-            lotateY = (accuracy * Float(pointDiff.y) + lotateY)
-            cameraNode.rotation = SCNVector4(x:1, y:0, z:0, w: lotateY)
-        }
+        lotateY = (accuracy * Float(pointDiff.y) + lotateY)
+        cameraNode.rotation = SCNVector4(x:1, y:0, z:0, w: lotateY)
+        
+        print(lotateX)
+        print(lotateY)
+        
         pointBefore = pointNow
     }
 
