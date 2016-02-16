0d1fe89f682533723e7d13021a47883113ee8e79
Nari <na.ju.rights@gmail.com>
2016-02-16 09:43:20 +0900

    add door button

---
+++ RoomView.swift
@@ -0,0 +1,89 @@
+//
+//  RoomView.swift
+//  SuumoKit
+//
+//  Created by NarimasaIwabuchi on 2016/02/14.
+//  Copyright © 2016年 Nari. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class RoomView: SCNView {
+    private let cameraNode = SCNNode()
+    private var pointBefore = CGPointZero
+    private var lotateX: Float = 0.0
+    private var lotateY: Float = 0.0
+    private var sphereNode = SCNNode()
+    private var pixelWidth: CGFloat = 0
+    private var pixelHeight: CGFloat = 0
+    private let accuracy: Float = Float(M_PI*2) / 1000.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(frame: CGRect, options: [String : AnyObject]?) {
+        super.init(frame: frame, options: options)
+    }
+    
+    
+    func setup(){
+        pixelWidth = UIScreen.mainScreen().bounds.width / 1125
+        pixelHeight = UIScreen.mainScreen().bounds.height / 2001
+        let scene = SCNScene()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        let sphereGeometry = SCNSphere(radius: 10.0)
+        sphereNode = SCNNode(geometry: sphereGeometry)
+        sphereNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
+        
+        sphereGeometry.firstMaterial?.diffuse.contents = UIImage(named: "01.jpg")
+        sphereGeometry.firstMaterial?.doubleSided = true
+        sphereGeometry.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(-1, 1 ,1);
+        sphereGeometry.firstMaterial?.diffuse.wrapS = SCNWrapMode.Repeat
+        scene.rootNode.addChildNode(sphereNode)
+        
+        let doorNode = SCNNode()
+        doorNode.geometry = SCNCylinder(radius: 0.5, height: 1)
+        doorNode.position = SCNVector3(0, 0, 9.5)
+        let door = SCNMaterial()
+        door.diffuse.contents = UIImage(named:"door.png")
+        doorNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
+        doorNode.rotation = SCNVector4(x: 0, y: 0, z: 1, w: Float(M_PI / 2))
+        doorNode.geometry!.firstMaterial = door
+        
+        sphereNode.addChildNode(doorNode)
+        
+        cameraNode.position = SCNVector3(x:0, y:0, z: 0)
+        
+        self.scene = scene
+        self.allowsCameraControl = false
+        self.backgroundColor = UIColor.grayColor()
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        pointBefore = (touch?.locationInView(self))!
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {                let touch = touches.first
+        let pointNow = (touch?.locationInView(self))!
+        let pointDiff = CGPointMake(pointNow.x - pointBefore.x, pointNow.y - pointBefore.y)
+        
+        lotateX = (accuracy * Float(pointDiff.x) + lotateX) % Float(M_PI*2)
+        sphereNode.rotation = SCNVector4(x:0, y:-1, z:0, w: lotateX)
+        
+        lotateY = (accuracy * Float(pointDiff.y) + lotateY)
+        cameraNode.rotation = SCNVector4(x:1, y:0, z:0, w: lotateY)
+        pointBefore = pointNow
+    }
+
+}
--- SuumoKit/SuumoViewController.swift
+++ SuumoKit/SuumoViewController.swift
@@ -11,83 +11,9 @@ import QuartzCore
 import SceneKit
 
 class SuumoViewController: UIViewController {
-
-    private let cameraNode = SCNNode()
-    private var pointBefore = CGPointZero
-    private var lotateX: Float = 0.0
-    private var lotateY: Float = 0.0
-    private var tubeNode = SCNNode()
-    private var pixelWidth: CGFloat = 0
-    private var pixelHeight: CGFloat = 0
-    private let accuracy: Float = Float(M_PI*2) / 1000.0
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        let rv = RoomView(frame: self.view.frame)
+        self.view.addSubview(rv)
     }
-    
-    func setup(){
-        pixelWidth = UIScreen.mainScreen().bounds.width / 1125
-        pixelHeight = UIScreen.mainScreen().bounds.height / 2001
-        let scene = SCNScene()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        let height = CGFloat(150)
-        let width = CGFloat(50)
-        
-//        let tubeGeometry = SCNTube(innerRadius: width, outerRadius: width, height: height)
-//        tubeNode = SCNNode(geometry: tubeGeometry)
-//        tubeNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
-//        tubeNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
-//        
-//        tubeGeometry.firstMaterial?.diffuse.contents = UIImage(named: "01.jpg")
-//        scene.rootNode.addChildNode(tubeNode)
-        
-        let tubeGeometry = SCNSphere(radius: 10.0)
-        tubeNode = SCNNode(geometry: tubeGeometry)
-        tubeNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
-        
-        tubeGeometry.firstMaterial?.diffuse.contents = UIImage(named: "01.jpg")
-        tubeGeometry.firstMaterial?.doubleSided = true
-        tubeGeometry.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(-1,1,1);
-        tubeGeometry.firstMaterial?.diffuse.wrapS = SCNWrapMode.Repeat
-        
-        scene.rootNode.addChildNode(tubeNode)
-        
-        
-        cameraNode.position = SCNVector3(x:0, y:0, z: 0)
-        
-        let scnView = self.view as! SCNView
-        scnView.scene = scene
-        scnView.allowsCameraControl = false
-        scnView.backgroundColor = UIColor.grayColor()
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let touch = touches.first
-        pointBefore = (touch?.locationInView(self.view))!
-    }
-    
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {                let touch = touches.first
-        let pointNow = (touch?.locationInView(self.view))!
-        let pointDiff = CGPointMake(pointNow.x - pointBefore.x, pointNow.y - pointBefore.y)
-        
-        lotateX = (accuracy * Float(pointDiff.x) + lotateX) % Float(M_PI*2)
-        tubeNode.rotation = SCNVector4(x:0, y:-1, z:0, w: lotateX)
-        
-        lotateY = (accuracy * Float(pointDiff.y) + lotateY)
-        cameraNode.rotation = SCNVector4(x:1, y:0, z:0, w: lotateY)
-        
-        print(lotateX)
-        print(lotateY)
-        
-        pointBefore = pointNow
-    }
-
 }
